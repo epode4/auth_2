5362ba94b1803d4b9dec238a2af97cb6f48e58ad
epode4 <epode4@gamil.com>
2023-08-29 16:12:18 +0900

    create 기능까지

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
+
 # Create your views here.
 def index(request):
     articles = Article.objects.all()
@@ -8,3 +10,24 @@ def index(request):
         'articles' : articles
     }
     return render(request, 'index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+
+        if form.is_valid():
+            article = form.save(commit=False)
+
+            article.user = request.user
+
+            article.save()
+
+            return redirect('articles:index')
+    else:
+        form = ArticleForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'form.html', context)
